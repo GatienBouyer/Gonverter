63bbd6a78c7cbc1054d282d7e00327de9773d33f
GatienBouyer <131701207+GatienBouyer@users.noreply.github.com>
2025-01-04 20:37:07 +0100

    hello world package

---
+++ src/gonverter/__main__.py
@@ -0,0 +1,15 @@
+"""
+Entry point of the application.
+
+Can be called with "python3 -m gonverter".
+
+"""
+
+
+def main() -> None:
+    """Entry point of the application."""
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
