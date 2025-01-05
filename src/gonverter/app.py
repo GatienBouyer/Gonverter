5767f6744fd6446de75f876e11f293f6ae27dcc1
GatienBouyer <131701207+GatienBouyer@users.noreply.github.com>
2025-01-05 16:29:50 +0100

    Add the UI v0

--- src/gonverter/app.py
+++ src/gonverter/app.py
@@ -2,6 +2,7 @@
 Definition of the application UI.
 """
 import tkinter
+from tkinter import scrolledtext
 from tkinter import ttk
 
 
@@ -19,9 +20,18 @@ def create_root() -> tkinter.Tk:
 
 def create_ui(root: tkinter.Misc) -> None:
     """Create the UI inside the root frame of the application."""
-    frm = ttk.Frame(root, padding=10)
-    frm.pack(expand=True, fill="both")
-    label = ttk.Label(frm, text="Hello World!")
-    label.pack(side="top", padx=5, pady=5)
-    button = ttk.Button(frm, text="Quit", command=root.destroy, name="quit")
-    button.pack(side="top", padx=5, pady=5)
+    frame = ttk.Frame(root, padding=5)
+    frame.pack(expand=True, fill="both")
+    frame.grid_columnconfigure(index=0, weight=1)
+    frame.grid_columnconfigure(index=1, weight=1)
+    frame.grid_rowconfigure(index=1, weight=1)
+    label_input = ttk.Label(frame, text="Your text", padding=5)
+    label_input.grid(column=0, row=0)
+    text_input = scrolledtext.ScrolledText(frame)
+    text_input.grid(column=0, row=1, sticky="NSWE", padx=5, pady=5)
+    label_output = ttk.Label(frame, text="Result", padding=5)
+    label_output.grid(column=1, row=0)
+    text_output = scrolledtext.ScrolledText(frame, state="disabled")
+    text_output.grid(column=1, row=1, sticky="NSWE", padx=5, pady=5)
+    convert_button = ttk.Button(frame, text="Convert", padding=5)
+    convert_button.grid(column=0, columnspan=2, row=2)
