f128061773d0623306ea7fc20a2770a5f3fbd95e
GatienBouyer <131701207+GatienBouyer@users.noreply.github.com>
2025-01-05 18:30:06 +0100

    Complete unit tests

--- test/test_base64.py
+++ test/test_base64.py
@@ -1,13 +1,18 @@
+"""Tests for the base64 encoding conversions."""
 import unittest
 
 from gonverter import base64
 
 
-class Test(unittest.TestCase):
+class Test(unittest.TestCase):  # pylint: disable=missing-class-docstring
     def test_base64_to_utf8(self) -> None:
+        """Test the base64.base64_to_utf8 function."""
+        self.assertEqual(base64.base64_to_utf8(""), "")
         self.assertEqual(base64.base64_to_utf8("YQ=="), "a")
 
     def test_utf8_to_base64(self) -> None:
+        """Test the base64.utf8_to_base64 function."""
+        self.assertEqual(base64.utf8_to_base64(""), "")
         self.assertEqual(base64.utf8_to_base64("a"), "YQ==")
 
 
